bc4b77b86db6e90831513f95c5ddabbfb1a45fc5
JonathanMauro <contatojmauro@hotmail.com>
2022-08-15 17:05:17 -0300

    inserindo os estudos de ML

--- CursoemVideo/Ex23.py
+++ CursoemVideo/Ex23.py
@@ -1,2 +1,19 @@
 #Exercício Python 23: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
 
+numero = int(input('Informe um número: '))
+#O metodo abaixo dará erro caso coloque menos do que quatro números
+#n = str(numero)
+print('Analisando o número {}'.format(numero))
+#print('Unidade: {}'.format(n[3]))
+#print('Dezena: {}'.format(n[2]))
+#print('Centena: {}'.format(n[1]))
+#print('Milhar: {}'.format(n[0]))
+
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
